f2888dd4120525437178e7051e99ec4df636d55a
zedsalim <salimzaiidi@gmail.com>
2023-07-15 14:17:27 +0000

    add help_notification.sh

---
+++ config/scripts/help_notification.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Display the startup notification
+notify-send --expire-time=10000000 --urgency=critical "IMPORTANT !" "
+Press Super + Shift + h to view the Guide 
+
+NOTE: The 'Super' Key is the 'Windows' Key
+
+
+(Click me and I disappear)"
+
+
