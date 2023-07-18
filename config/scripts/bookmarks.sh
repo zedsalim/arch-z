348e9d827d31920b3a648b5aa3a5a6c60742f689
zedsalim <salimzaiidi@gmail.com>
2023-07-18 21:13:40 +0100

    updates

--- config/scripts/bookmarks.sh
+++ config/scripts/bookmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 choice=$(cat /mnt/FILES/My_Stuff/bookmarks.txt | rofi -dmenu -l 30 -p "Choose Website: ")
-[ -z "$choice" ] && exit 0 || echo "$choice" | xargs "$BROWSER" || echo "$choice" | xargs brave
+[ -z "$choice" ] && exit 0 || echo "$choice" | xargs "$BROWSER" || echo "$choice" | xargs chromium
 
 
 sleep 1 && exit
