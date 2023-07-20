80200dc5df1500a4293f023047cbf4735dc8a6b7
zedsalim <salimzaiidi@gmail.com>
2023-07-20 18:09:26 +0100

    update

--- optional/nvim-desktop.sh
+++ optional/nvim-desktop.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
 
+# sudo vim /usr/share/applications/nvim.desktop
+# Exec=kitty -e sh -c "nvim %F"
 sudo su <<EOF
 cp /home/zed/arch-z/optional/nvim.desktop ~/.local/share/applications/nvim.desktop
 exit
