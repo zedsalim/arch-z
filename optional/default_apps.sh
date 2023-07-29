5aa05af8a75ba53d30db764f0a37082188fb4f41
zedsalim <salimzaiidi@gmail.com>
2023-07-29 06:09:02 +0100

    update to sxiv

--- optional/default_apps.sh
+++ optional/default_apps.sh
@@ -8,8 +8,8 @@ xdg-mime default code-oss.desktop text/css
 xdg-mime default nvim.desktop text/x-shellscript
 xdg-mime default nvim.desktop text/markdown
 xdg-mime default nvim.desktop text/x-csrc
-xdg-mime default viewnior.desktop image/png
-xdg-mime default viewnior.desktop image/jpg
-xdg-mime default viewnior.desktop image/jpeg
-xdg-mime default viewnior.desktop image/JPEG
-xdg-mime default viewnior.desktop image/svg+xml
+xdg-mime default sxiv.desktop image/png
+xdg-mime default sxiv.desktop image/jpg
+xdg-mime default sxiv.desktop image/jpeg
+xdg-mime default sxiv.desktop image/JPEG
+xdg-mime default sxiv.desktop image/svg+xml
