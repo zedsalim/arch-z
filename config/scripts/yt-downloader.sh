62c707770e4c9983b974262b12ceb94bf7e26217
zedsalim <salimzaiidi@gmail.com>
2023-08-02 10:21:56 +0100

    add playlist download

--- config/scripts/yt-downloader.sh
+++ config/scripts/yt-downloader.sh
@@ -11,6 +11,13 @@ download_video() {
     yt-dlp -f "bestvideo[height<=$quality][ext=mp4]+bestaudio[ext=m4a]/best[height<=$quality][ext=mp4]/best" -o "%(title)s.%(ext)s" "$url"
 }
 
+# Function to download playlist
+download_playlist() {
+    local url="$1"
+    echo "Downloading playlist..."
+    yt-dlp -o "%(playlist_index)s - %(title)s.%(ext)s" "$url"
+}
+
 # Check if xclip is installed
 if ! command -v xclip &> /dev/null; then
     echo "xclip is not installed. Please install it using 'sudo apt-get install xclip' (for Ubuntu/Debian) or appropriate package manager for your distribution."
@@ -25,14 +32,24 @@ if [ -z "$url" ]; then
     exit 1
 fi
 
-preferred_quality=720
+echo "Do you want to download a single video or a playlist?"
+echo "Enter '1' for a single video or '2' for a playlist: "
+read choice
 
-# Check if the video is available in the preferred quality
-yt-dlp -F "$url" | grep -q "$preferred_quality"
+if [ "$choice" = "1" ]; then
+    preferred_quality=720
 
-if [ $? -eq 0 ]; then
-    download_video "$url" "$preferred_quality"
+    # Check if the video is available in the preferred quality
+    yt-dlp -F "$url" | grep -q "$preferred_quality"
+
+    if [ $? -eq 0 ]; then
+        download_video "$url" "$preferred_quality"
+    else
+        echo "720p resolution not available. Downloading in 480p..."
+        download_video "$url" 480
+    fi
+elif [ "$choice" = "2" ]; then
+    download_playlist "$url"
 else
-    echo "720p resolution not available. Downloading in 480p..."
-    download_video "$url" 480
+    echo "Invalid choice. Please enter '1' or '2'."
 fi
