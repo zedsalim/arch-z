aad14c6a2ee2963e027420934724bbe0540bd29f
zedsalim <salimzaiidi@gmail.com>
2023-08-02 12:14:53 +0100

    commentig stuff

--- 4-rice.sh
+++ 4-rice.sh
@@ -20,12 +20,12 @@ cp -r config/wallpapers/ ~/.config
 cp -r config/i3/ ~/.config
 cp -r config/sxhkd/ ~/.config
 cp -r config/scripts/ ~/.config
-cp -r config/alacritty/ ~/.config
+# cp -r config/alacritty/ ~/.config
 cp -r config/dunst/ ~/.config
 cp -r config/rofi/ ~/.config
 cp -r config/ranger/ ~/.config
 cp -r config/polybar/ ~/.config
-cp -r config/pcmanfm/ ~/.config
+# cp -r config/pcmanfm/ ~/.config
 cp -r config/zathura/ ~/.config
 cp -r config/kitty/ ~/.config
 cp config/picom.conf ~/.config
@@ -53,7 +53,7 @@ cp config/.gtkrc-2.0 ~/
 cd
 cd arch-z/
 source optional/keyboard_layout.sh
-source optional/terminal_in_pcmanfm.sh
+# source optional/terminal_in_pcmanfm.sh
 source optional/nvim.sh
 source optional/default_apps.sh
 source optional/nvim-desktop.sh
