d38028977aced3fd513faa0a9753201cb59eb841
zedsalim <salimzaiidi@gmail.com>
2023-08-07 10:13:01 +0100

    update

--- config/scripts/yt-downloader.sh
+++ config/scripts/yt-downloader.sh
@@ -3,6 +3,13 @@
 
 cd /home/zed/Downloads/
 
+# Function to prompt user for choice
+get_choice() {
+    local choice
+    read -p "Do you want to download a single video or a playlist? (1/2): " choice
+    echo "$choice"
+}
+
 # Function to download video or playlist
 download() {
     local url="$1"
@@ -17,56 +24,41 @@ download() {
 # Read the video URL from the clipboard
 url=$(xclip -o -selection clipboard)
 
+# Get available formats
+available_formats=$(yt-dlp -F "$url")
+
 # Check for audio quality
 audio="599"
-yt-dlp -F "$url" | grep -q "599"
-if [ $? -ne 0 ]; then
+if ! echo "$available_formats" | grep -q "599"; then
     audio="bestaudio"
 fi
 
 # Check for video quality options
-video_quality="299"
 output_format_video="%(title)s.%(ext)s"
 output_format_playlist="%(playlist_index)s - %(title)s.%(ext)s"
 
-yt-dlp -F "$url" | grep -q "299"
-if [ $? -eq 0 ]; then
-    echo "Do you want to download a single video or a playlist?"
-    echo "Enter '1' for a single video or '2' for a playlist: "
-    read choice
+choice=$(get_choice)
 
-    if [ "$choice" = "1" ]; then
-        download "$url" "$video_quality" "$audio" "$output_format_video"
-    elif [ "$choice" = "2" ]; then
-        download "$url" "$video_quality" "$audio" "$output_format_playlist"
-    else
+case "$choice" in
+    1)
+        if echo "$available_formats" | grep -q "299"; then
+            download "$url" "299" "$audio" "$output_format_video"
+        elif echo "$available_formats" | grep -q "302"; then
+            download "$url" "302" "$audio" "$output_format_video"
+        else
+            download "$url" "bestvideo" "$audio" "$output_format_video"
+        fi
+        ;;
+    2)
+        if echo "$available_formats" | grep -q "299"; then
+            download "$url" "299" "$audio" "$output_format_playlist"
+        elif echo "$available_formats" | grep -q "302"; then
+            download "$url" "302" "$audio" "$output_format_playlist"
+        else
+            download "$url" "bestvideo" "$audio" "$output_format_playlist"
+        fi
+        ;;
+    *)
         echo "Invalid choice. Please enter '1' or '2'."
-    fi
-elif yt-dlp -F "$url" | grep -q "302"; then
-    video_quality="302"
-    echo "Do you want to download a single video or a playlist?"
-    echo "Enter '1' for a single video or '2' for a playlist: "
-    read choice
-
-    if [ "$choice" = "1" ]; then
-        download "$url" "$video_quality" "$audio" "$output_format_video"
-    elif [ "$choice" = "2" ]; then
-        download "$url" "$video_quality" "$audio" "$output_format_playlist"
-    else
-        echo "Invalid choice. Please enter '1' or '2'."
-    fi
-else
-    echo "Quality options not available. Downloading best available quality..."
-    video_quality="bestvideo"
-    echo "Do you want to download a single video or a playlist?"
-    echo "Enter '1' for a single video or '2' for a playlist: "
-    read choice
-
-    if [ "$choice" = "1" ]; then
-        download "$url" "$video_quality" "$audio" "$output_format_video"
-    elif [ "$choice" = "2" ]; then
-        download "$url" "$video_quality" "$audio" "$output_format_playlist"
-    else
-        echo "Invalid choice. Please enter '1' or '2'."
-    fi
-fi
+        ;;
+esac
