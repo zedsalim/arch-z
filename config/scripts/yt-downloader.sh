fcd05d255a32fd76aa84007621594634cc1f925b
zedsalim <salimzaiidi@gmail.com>
2023-08-07 11:50:42 +0100

    update

--- config/scripts/yt-downloader.sh
+++ config/scripts/yt-downloader.sh
@@ -13,25 +13,14 @@ get_choice() {
 # Function to download video or playlist
 download() {
     local url="$1"
-    local video_quality="$2"
-    local audio_quality="$3"
-    local output_format="$4"
+    local output_format="$2"
 
     echo "Downloading..."
-    yt-dlp -f "$video_quality+$audio_quality" -o "$output_format" "$url"
+    yt-dlp -f "b" -S "filesize~50M" -o "$output_format" "$url"
 }
 
 # Read the video URL from the clipboard
 url=$(xclip -o -selection clipboard)
-
-# Get available formats
-available_formats=$(yt-dlp -F "$url")
-
-# Check for audio quality
-audio="599"
-if ! echo "$available_formats" | grep -q "599"; then
-    audio="bestaudio"
-fi
 
 # Check for video quality options
 output_format_video="%(title)s.%(ext)s"
@@ -41,22 +30,10 @@ choice=$(get_choice)
 
 case "$choice" in
     1)
-        if echo "$available_formats" | grep -q "299"; then
-            download "$url" "299" "$audio" "$output_format_video"
-        elif echo "$available_formats" | grep -q "302"; then
-            download "$url" "302" "$audio" "$output_format_video"
-        else
-            download "$url" "bestvideo" "$audio" "$output_format_video"
-        fi
+        download "$url" "$output_format_video"
         ;;
     2)
-        if echo "$available_formats" | grep -q "299"; then
-            download "$url" "299" "$audio" "$output_format_playlist"
-        elif echo "$available_formats" | grep -q "302"; then
-            download "$url" "302" "$audio" "$output_format_playlist"
-        else
-            download "$url" "bestvideo" "$audio" "$output_format_playlist"
-        fi
+        download "$url" "$output_format_playlist"
         ;;
     *)
         echo "Invalid choice. Please enter '1' or '2'."
