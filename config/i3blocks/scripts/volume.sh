2db65bec086051b43cec4e6e7e7e028ffa2e7a29
zedsalim <salimzaiidi@gmail.com>
2023-08-21 17:46:34 +0100

    update

--- config/i3blocks/scripts/volume.sh
+++ config/i3blocks/scripts/volume.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Get volume percentage and mute status
-volume_info=$(amixer get Master | grep -oE '[0-9]+%' | head -n 1)
-mute_status=$(amixer get Master | grep -oE '\[(on|off)\]' | head -n 1)
+volume_info=$(pactl list sinks | grep 'Volume:' | awk 'NR==3{print $5}' | sed 's/%//')
+mute_status=$(pactl list sinks | grep 'Mute:' | awk 'NR==2{print $2}')
 
-if [ "$mute_status" = "[off]" ]; then
+if [ "$mute_status" = "yes" ]; then
     echo "🔇"
 else
-    echo "VOL: $volume_info"
+    echo "VOL: $volume_info%"
 fi
