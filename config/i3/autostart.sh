13143c26a21c9a60aff5b50488a99f0e35092ae8
zedsalim <salimzaiidi@gmail.com>
2023-10-09 07:20:49 +0100

    update the caps lock keys

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -26,6 +26,7 @@ ibus-daemon -drx &
 sleep 2
 ibus engine xkb:fr:latin9:fra &
 
-setxkbmap -option "caps:escape_shifted_capslock" &
+# setxkbmap -option "caps:escape_shifted_capslock" &
+setxkbmap -option "caps:escape" &
 
 xset r rate 300 50
