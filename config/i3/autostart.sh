e74ed5373a50d3d4b2239a067abc08139bcf62aa
zedsalim <salimzaiidi@gmail.com>
2024-02-10 10:06:52 +0100

    Add nvidia auto fan speed

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,6 +1,8 @@
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
+nvidia-settings -a "[gpu:0]/GPUFanControlState=1" -a "[fan:0]/GPUTargetFanSpeed=20" -a "[fan:1]/GPUTargetFanSpeed=20" &
+
 picom --config ~/.config/picom.conf &
 dunst &
 
