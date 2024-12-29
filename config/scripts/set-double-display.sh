8f1a78f7780343e89aa1adce4f93fc58b0319989
zedsalim <salimzaiidi@gmail.com>
2024-12-29 08:07:35 +0100

    Update display scripts with warmer colors and reduced brightness

--- config/scripts/set-double-display.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 900x0 --rotate normal --output HDMI-0 --mode 1600x900 --pos 0x0 --rotate right --output DP-0 --off --output DP-1 --off
-
-source ~/.config/scripts/last-single-wallpapers.sh
-
-# source ~/.config/scripts/last-two-wallpapers.sh
-
-# i3-msg restart
--- config/scripts/set-single-display.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --off --output DP-0 --off --output DP-1 --off
-
-source ~/.config/scripts/last-single-wallpapers.sh
-
-# i3-msg restart
